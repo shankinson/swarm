25e5c6a1a2f8bde3bb1925fe479c18134d183cc5
shankinson <shankinson@gmail.com>
2024-10-25 18:14:31 -0300

    Let agents define a response format

--- swarm/types.py
+++ swarm/types.py
@@ -18,6 +18,7 @@ class Agent(BaseModel):
     functions: List[AgentFunction] = []
     tool_choice: str = None
     parallel_tool_calls: bool = True
+    response_format: dict = None
 
 
 class Response(BaseModel):
